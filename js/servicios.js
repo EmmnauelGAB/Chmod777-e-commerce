8a95ddbf4bddaaac7ddacb535c5ac3bd566d7790
NenemyVc <nenemy28@gmail.com>
2022-03-16 21:58:56 -0600

    Se cambian imagenes del bloque SERVICIOS y si agustan estilos de boton addServicios e index

--- js/servicios.js
+++ js/servicios.js
@@ -2,27 +2,27 @@
 
 let servicios =[
     {
-       src : `../img/servicios/individual.jpg`,
+       src : `../img/servicios/terapia-individual.jpg`,
        title: "Psicoterapia individual",
        desc : "Cuando te encuentras en una situacion difícil de sobrellevar, no es facil pedir ayuda, pero cuando te encuentras un espacio para ti en terapia, se vuelve un momento para crecer y descubrirte desde la profundidad. Desde ahí, trabajaremos juntos."
     },
     {
-      src : `../img/servicios/pareja.jpg`,
+      src : `../img/servicios/terapia-parejas.jpg`,
       title: "Psicoterapia de pareja",
       desc : "Las dificultades en la pareja son una etapa en toda relación, acudir a terapia puede representar la oportunidad de escucharse y entenderse desde una comunicación que les permita verse más allá de lo que viven en el presente."
    },
    {
-    src : `../img/servicios/adolescente.png`,
+    src : `../img/servicios/terapia-adolescentes.jpg`,
     title: "Psicoterapia de adolescentes",
     desc : " Las etapas de crecimiento de la vida son mejores cuando se tiene el espacio para poder experimentarlas y entenderlas; la adolescencia es una etapa crucial para el desarrollo tanto del paciente como de sus familiares."
   },
   {
-    src : `../img/servicios/grupal.jpg`,
+    src : `../img/servicios/terapia-grupal.jpg`,
     title: "Psicoterapia grupal",
     desc : "El trabajo clínico de uno, se complementa a través de la experiencia y el trabajo de todos. Es así como el grupo es curativo en sí mismo, y qué mejor con la compañía de un especialista que puede guiarlos a descubrirse mucho mejor."
   },
   {
-    src : `../img/servicios/mamas.jpg`,
+    src : `../img/servicios/terapia-grupal-mamas.jpg`,
     title: "Psicoterapia grupal para mamás",
     desc : "Es un espacio diseñado para el trabajo clínico de la maternidad y de lo que implica ser y vivirse como madres y mujeres; se trabaja con las vivencias, aprendizajes y dificultades que el maternar conlleva. "
   }
@@ -37,7 +37,7 @@ let servicios =[
     let myCard= `
     <div class="col mb-4"
       <div class="card">
-              <img src="${src}" class="card-img-top" alt="...">
+              <img src="${src}" class="card-img-top alt="...">
             <div class="card-body card-bodyy">
               <h5 class="card-titles">${title}</h5>
               <p class="card-texts">${desc}</p>
@@ -93,12 +93,12 @@ let servicios =[
        descT : "Si va a tener su primer hijo, es muy probable que se sienta abrumada por las preguntas, los miedos o, simplemente, por no saber qué esperar. Por eso para muchos padres primerizos tomar clases de preparación para el parto les ayuda a calmar sus preocupaciones y responder a muchas preguntas, te ayudaré a resolver tus dudas."
     },
     {
-      srcT : `../img/servicios/tallerpareja.jpeg`,
+      srcT : `../img/servicios/taller-parejas.jpg`,
       titleT : "Taller parejas",
       descT : "Tienen por objetivo apoyar a la pareja a conocerse mejor, a fin de que puedan comprenderse, humanizarse y apoyarse amorosamente en sus áreas disfuncionales, cada taller tiene una temática especial con el propósito de desarrollar diferentes habilidades a fin de potencializar el proceso terapéutico de la pareja."
     },
     {
-      srcT : `../img/servicios/crianza.jpg`,
+      srcT : `../img/servicios/crianza-positiva.jpg`,
       titleT : "Taller crianza positiva",
       descT : "Conversatorios y conferencias para madres y padres sobre crianza, educación, maternidad y paternidad. Talleres de información para una maternidad y paternidad más feliz.  Organizamos talleres extraordinarios sobre otros temas de interés como adolescencia, celos entre hermanos."
     },
@@ -108,7 +108,7 @@ let servicios =[
       descT : "El inicio en el consumo se produce a edades muy tempranas, entre 13 y 16 años dependiendo de la sustancia. Por lo que el trabajo preventivo hay que empezarlo en edades anteriores. El alcohol, el tabaco y el cannabis son las sustancias más consumidas en la actualidad."
     },
     {
-      srcT : `../img/servicios/educativo.jpg`,
+      srcT : `../img/servicios/taller-psicoeducativo.jpg`,
       titleT : "Taller psicoeducativo",
       descT : "Dependiendo de las necesidades del grupo, se determinan las horas y número de sesiones para trabajar el tema. Este apartado está disponible para grupos de instituciones educativas u organizaciones."
     }
