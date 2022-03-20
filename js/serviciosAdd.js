0d2bd8451c1da1d41649f70967e209ff11986b4e
jcc <correajocelin8@gmail.com>
2022-03-19 18:54:09 -0600

    Fix - Validaciones con Bootstrap y guardar en Local Storage

--- js/serviciosAdd.js
+++ js/serviciosAdd.js
@@ -1,3 +1,18 @@
+
+
+let servicios =[
+ {
+    "_id": 1,
+    "titulo": "Terapia individual",
+    "tipo": "Terapia",
+    "descripcion": "Cuando te encuentras en una situacion difícil de sobrellevar, no es facil pedir ayuda, pero cuando te encuentras un espacio para ti en terapia, se vuelve un momento para crecer y descubrirte desde la profundidad. Desde ahí, trabajaremos juntos.",
+    "imagen": "terapia-individual.png"
+}
+];
+
+let jsonServicios = JSON.stringify(servicios);
+localStorage.setItem("servicios",jsonServicios);
+
 //**valida cada uno de los campos del form */
 
 function serviciosAdd(titulo, tipo, description, imagen) {
@@ -9,44 +24,89 @@ function serviciosAdd(titulo, tipo, description, imagen) {
     // valida titulo
 
     let text = /^[a-zA-ZÀ-ÿ\s]{1,40}$/;
+    let textName;
+    let condicion = true;
     if (tituloValor === "" || tituloValor.length <= 10 || !text.test(tituloValor)) {
-        titulo.classList.add("is-invalid");
+        textName = `<div class="alert alert-danger" role="alert">¡Título inválido! </div>`;
+        condicion = false;
     } else {
-        titulo.classList.add("is-valid");
+        textName = `<div class="alert alert-success" role="alert">¡Título válido!</div>`;  
     }
+    document.getElementById("serviceDemo").innerHTML = textName;
 
     // valida tipo
 
     if (tipoValor === "") {
-        tipo.classList.add("is-invalid");
+        textName = `<div class="alert alert-danger" role="alert">¡Elige un tipo de servicio! </div>`;
+        condicion = false;
     } else {
-        tipo.classList.add("is-valid");
+        textName = `<div class="alert alert-success" role="alert">¡Tipo de servicio válido!</div>`;
     }
+    document.getElementById("tipoDemo").innerHTML = textName;
 
     // valida Descripcion
-    let texto = /^[a-zA-ZÀ-ÿ\s]{1,1000}$/;
-    if (descriptionValor === "" || descriptionValor.length <=20 || !texto.test(descriptionValor)) {
-        description.classList.add("is-invalid");
+    if (descriptionValor === "" || descriptionValor.length <= 20 || /^\s+$/.test(description)) {
+        textName = `<div class="alert alert-danger" role="alert">¡Ingresa una descripción! </div>`;
+        condicion = false;
     } else {
-        description.classList.add("is-valid");
+        textName = `<div class="alert alert-success" role="alert">¡Descripción válida!</div>`;
     }
+    document.getElementById("descriptionDemo").innerHTML = textName;
 
     // valida imagen
     if (imagenValor === "") {
-        imagen.classList.add("is-invalid");
+        textName = `<div class="alert alert-danger" role="alert">¡Agrega una imagen! </div>`;
+        condicion = false;
     } else {
-        imagen.classList.add("is-valid");
+        textName = `<div class="alert alert-success" role="alert">¡Imagen válida!</div>`;
+    }
+
+    document.getElementById("imagenDemo").innerHTML = textName;
+
+    if (condicion = true) {
+        let nuevoServicio = {
+            "_id": arrayServicios.length + 1,
+            "titulo": titulo.value,
+            "tipo": tipo.value,
+            "descripcion": description.value,
+            "imagen": imagen.value
+        };
+        arrayServicios.push(nuevoServicio);
+        let jsonServicios = JSON.stringify(arrayServicios);
+        localStorage.setItem("servicios", jsonServicios);
+        titulo.value = "";
+        tipo.value = "";
+        description.value = "";
+        imagen.value = "";
+        Swal.fire({
+            icon: 'success',
+            title: '¡Éxito!',
+            text: '¡Se agregó el servicio!',
+        })
+    }else{
+        Swal.fire({
+            icon: 'error',
+            title: '¡Falló!',
+            text: '¡No se agregó el servicio!',
+        })
     }
 }
 
-const formAddServicio = document.getElementById("form-servicio");
+let serviciosStorage = localStorage.getItem("servicios");
+let arrayServicios = JSON.parse(serviciosStorage);
+console.log(arrayServicios);
 
-formAddServicio.addEventListener("submit", (evento) => {
+//(Borra todo lo de Local Storage)
+//window.localStorage.removeItem("servicios")   
+
+let formAddServicio = document.getElementById("form-servicio");
+
+formAddServicio.addEventListener("submit", function (evento) {
     let titulo = document.getElementById("newService");
     let tipo = document.getElementById("servicioTipo");
     let description = document.getElementById("description");
     let imagen = document.getElementById("validatedCustomFile");
     evento.preventDefault();
     serviciosAdd(titulo, tipo, description, imagen);
-})
+});
 
